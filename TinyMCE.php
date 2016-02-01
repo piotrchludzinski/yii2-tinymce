9a37bf9389662304f231c84c89b0f3ac8915fa96
Piotr Chludziński <chludzinski@onlineventure.pl>
2016-02-01 12:06:19 +0100

    adding plugin charactercount

--- TinyMCE.php
+++ TinyMCE.php
@@ -59,7 +59,7 @@ class TinyMCE extends InputWidget
 	 */
 	public $plugins = [
 		'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-		'searchreplace wordcount visualblocks visualchars code fullscreen',
+		'searchreplace charactercount wordcount visualblocks visualchars code fullscreen',
 		'insertdatetime media nonbreaking save table contextmenu directionality',
 		'emoticons template paste textcolor',
 	];
